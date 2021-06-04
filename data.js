2e070d4e08f6066d1754f5b10c4180fa299ee2d9
Jitka Badhan Nebeska <jitkabadhan@gmail.com>
2021-06-05 00:07:31 +0200

    Data structure in data.js (not complete)

---
+++ data.js
@@ -0,0 +1,90 @@
+const data = {
+  intro : [
+    {
+      vers1 : '',
+      vers2 : '',
+      vers3 : '',
+      vers4 : '',
+      vers5 : '',
+      vers6 : '',
+    },
+    {
+      vers1 : '',
+      vers2 : '',
+      vers3 : '',
+      vers4 : '',
+      vers5 : '',
+      vers6 : '',
+    },
+    {
+      vers1 : '',
+      vers2 : '',
+      vers3 : '',
+      vers4 : '',
+      vers5 : '',
+      vers6 : '',
+    },
+    {
+      vers1 : '',
+      vers2 : '',
+      vers3 : '',
+      vers4 : '',
+      vers5 : '',
+      vers6 : '',
+    },
+  ],
+  buildings : [
+    {
+      name : 'Hospoda',
+      mapImg : '',
+      cardImg : '',
+      function : 'quizz',
+      description : '',
+      quizz : {
+        isPresent : true;
+        name : '',
+        pic : '',
+        text : '',
+        answers : [
+          {
+            button : 'A',
+            text : '',
+            pic : '',
+          }, 
+          {
+            button : 'B',
+            text : '',
+            pic : '',
+          },
+          {
+            button : 'C',
+            text : '',
+            pic : '',
+          },
+          {
+            button : 'D',
+            text : '',
+            pic : '',
+          }, 
+        ],
+        solution : '',
+        hint : {
+          text : '',
+          pic : '',
+        },
+        isSolved : false,
+        result : {
+          ifCorrect : '',
+          ifLessCorrect : '',
+          ifNotCorrect : '',
+        }
+      },
+      indication : {
+        isPresent : false,
+        pic : '',
+        description : '',
+      },
+      isActive : false,
+    },
+  ]
+}
